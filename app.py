e0f1524cfb1d2334469383f9c2673e0f13a42e4f
csteducationindia-design <csteducationindia@gmail.com>
2026-01-17 15:57:54 +0530

    up

--- app.py
+++ app.py
@@ -989,4 +989,4 @@ def initialize_database():
 
 if __name__ == '__main__':
     initialize_database()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
